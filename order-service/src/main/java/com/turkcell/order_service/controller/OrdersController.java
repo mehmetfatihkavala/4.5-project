3c9c02d2e4af51a4a19409a3002ce10b18855e12
mehmetfatihkavala <mehmetfatihkavala21@gmail.com>
2025-12-27 16:56:19 +0300

    feat: Publish order creation events to Kafka and update Kafka port and network configuration.

--- order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -1,5 +1,8 @@
 package com.turkcell.order_service.controller;
 
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/orders")
 public class OrdersController {
 
+    private final StreamBridge streamBridge;
+
+    public OrdersController(StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
+    }
+
     @PostMapping
     public String createOrder(@RequestBody CreateOrderDto createOrderDto) {
+        OrderCreatedEvent event = new OrderCreatedEvent(createOrderDto.productId());
+
+        Message<OrderCreatedEvent> message = MessageBuilder.withPayload(event).build();
+
+        streamBridge.send("orderCreated-out-0", message);
         return createOrderDto.productId();
     }
 
     public record CreateOrderDto(String productId) {
+    }
+
+    public record OrderCreatedEvent(String productId) {
     }
 }
