9c9fa27934008fe6feb9a649a0800827accf4233
mehmetfatihkavala <mehmetfatihkavala21@gmail.com>
2025-12-27 16:13:01 +0300

    add kafka config

--- product-service/src/main/java/com/turkcell/product_service/application/dtos/ProductResponse.java
+++ product-service/src/main/java/com/turkcell/product_service/application/dtos/ProductResponse.java
@@ -1,14 +1,12 @@
 package com.turkcell.product_service.application.dtos;
 
-import java.math.BigDecimal;
-
 /**
  * Response DTO for a single product.
  */
 public record ProductResponse(
-        String id,
-        String name,
-        String description,
-        PriceDto price,
-        StockDto stock) {
+                String id,
+                String name,
+                String description,
+                PriceDto price,
+                StockDto stock) {
 }
