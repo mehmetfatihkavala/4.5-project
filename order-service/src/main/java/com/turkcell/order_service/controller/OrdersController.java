f56847d9f5c2f8e9a22728b7a9cb8d17a00dad13
mehmetfatihkavala <mehmetfatihkavala21@gmail.com>
2025-12-28 17:53:45 +0300

    feat: Implement outbox pattern for reliable event publishing with a scheduled relayer.

--- order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/turkcell/order_service/controller/OrdersController.java
@@ -1,42 +1,58 @@
 package com.turkcell.order_service.controller;
 
-import org.springframework.cloud.stream.function.StreamBridge;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.turkcell.order_service.messaging.outbox.OutboxMessage;
+import com.turkcell.order_service.messaging.outbox.OutboxRepository;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrdersController {
+    private final OutboxRepository outboxRepository;
+    private final ObjectMapper objectMapper;
 
-    private final StreamBridge streamBridge;
-
-    public OrdersController(StreamBridge streamBridge) {
-        this.streamBridge = streamBridge;
+    public OrdersController(OutboxRepository outboxRepository, ObjectMapper objectMapper) {
+        this.outboxRepository = outboxRepository;
+        this.objectMapper = objectMapper;
     }
 
-    @PostMapping
-    public String createOrder(@RequestBody CreateOrderDto createOrderDto) {
-        OrderCreatedEvent event = new OrderCreatedEvent(createOrderDto.productId());
+    @PostMapping()
+    public String createOrder(@RequestBody CreateOrderDto dto) throws JsonProcessingException {
+        /// .........
+        OrderCreatedEvent event = new OrderCreatedEvent(dto.productId());
 
-        Message<OrderCreatedEvent> message = MessageBuilder.withPayload(event).build();
+        OutboxMessage outboxMessage = new OutboxMessage();
+        outboxMessage.setAggregateId(UUID.randomUUID()); // normalde db'de oluşan order'ın idsi
+        outboxMessage.setAggregateType("Order");
+        outboxMessage.setEventId(UUID.randomUUID());
+        outboxMessage.setEventType("OrderCreatedEvent");
+        outboxMessage.setPayloadJson(objectMapper.writeValueAsString(event));
+        outboxRepository.save(outboxMessage);
 
-        try {
-            boolean isSent = streamBridge.send("orderCreated-out-0", message);
-            if (!isSent) {
-                System.out.println("Mesaj Gönderilemedi...");
-            }
-        } catch (Exception e) {
-            System.out.println("Mesaj Gönderilemedi...");
-        }
+        // Message<OrderCreatedEvent> message =
+        // MessageBuilder.withPayload(event).build();
+        /*
+         * try {
+         * boolean isSent = streamBridge.send("orderCreated-out-0", message);
+         * if (!isSent)
+         * System.out.println("Mesaj gönderilemedi");
+         * } catch (Exception e) {
+         * System.out.println("Mesaj gönderilemedi");
+         * }
+         */
 
-        return createOrderDto.productId();
+        return dto.productId();
     }
 
-    public record CreateOrderDto(String productId) {
+    record CreateOrderDto(String productId) {
     }
 
     public record OrderCreatedEvent(String productId) {
