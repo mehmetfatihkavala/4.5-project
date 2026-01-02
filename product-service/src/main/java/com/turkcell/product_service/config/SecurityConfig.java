3098c5fc0577d83d2653affadb6aba89931b45c1
mehmetfatihkavala <mehmetfatihkavala21@gmail.com>
2026-01-02 20:38:32 +0300

    feat: Add BFF gateway relayer with OAuth2 WebClient and secure product service with JWT authentication.

---
+++ product-service/src/main/java/com/turkcell/product_service/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.turkcell.product_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())) // Jwt filter yerine oauth2
+                .build(); // server kullanarak jwt kontrolü yapar
+    }
+}
